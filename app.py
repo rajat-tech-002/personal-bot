c24ddbe451fb44a109d47514eef83b4d1054214a
unknown <rajat.tech.002@gmail.com>
2022-12-23 13:50:47 +0530

    Initial commit

--- app.py
+++ app.py
@@ -18,4 +18,5 @@ def predict():
     return jsonify(message)       
 
 if __name__ == "__main__":
-    app.run(debug = True)    
+    # app.run(debug = True)
+    app.run(debug = False, host = '0.0.0.0')    
